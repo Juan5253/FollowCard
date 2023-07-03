7f88b1af00cbdf62225464aa9e87232a7b76396e
Juan5253 <Juan_SebastianF@hotmail.com>
2023-07-02 23:42:47 -0500

    Basic template app

---
+++ followcard-ui/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ''
+import TwitterFollowCard from './Components/TwitterFollowCard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TwitterFollowCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ followcard-ui/src/Components/TwitterFollowCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TwitterFollowCard() {
+    return (
+        <span> FollowCard </span>
+    )
+}
+
+export default TwitterFollowCard
